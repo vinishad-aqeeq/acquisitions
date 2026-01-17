c10d75d339f24da1c66778780bc5d842abd9f995
vinishad-aqeeq <vinishad@aqeeq.io>
2026-01-17 12:54:21 +0530

    Eslint prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello");
+});
 
 export default app;
